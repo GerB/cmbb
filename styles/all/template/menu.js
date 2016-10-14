84ea6dc086b5d9b2f90d9cb97581158d176477b8
Ger Bruinsma <gerbruinsma@ktba.com>
2016-10-14 16:57:11 +0200

    Responsive start

---
+++ styles/all/template/menu.js
@@ -0,0 +1,28 @@
+ // MOBILE MENU
+jQuery( document ).ready( function() {
+	var hWindow = jQuery( window ).height();
+	jQuery( "#menu-trigger" ).click( function() {
+		jQuery( "html" ).toggleClass( "menu-active" );
+		jQuery( "#main-navigation" ).css( "height", hWindow );
+	} );
+	jQuery( "#content-wrapper" ).click( function() {
+		jQuery( "html" ).removeClass( "menu-active" );
+	} );
+	jQuery( window ).resize( function() {
+        var wWindow = jQuery( window ).width();
+        if (wWindow > 900) {
+            var hSet = 'auto';            
+        }
+        else {
+            var hSet = jQuery( window ).height();
+        }
+        jQuery( "#main-navigation" ).css( "height", hSet );
+	} );
+
+} );
+jQuery( window ).bind( "orientationchange", function( event ) {
+	jQuery( "#menu-trigger" ).click( function() {
+		jQuery( "#main-navigation" ).css( "height", screen.height );
+	} );
+} );
+
